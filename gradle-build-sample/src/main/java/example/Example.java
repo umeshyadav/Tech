156e46d5453f569efc0f02d2ddff2a536c42e0f0
Umesh Yadav <umeshyadav@umeshs-MacBook-Pro.local>
2017-10-29 13:17:30 -0400

    First commit

---
+++ gradle-build-sample/src/main/java/example/Example.java
@@ -0,0 +1,10 @@
+package example;
+
+
+public class Example {
+
+	public static void main(String[] args) {
+		System.out.println("Yo what's up human! Oh you think you're fancy");
+	}
+
+}
---
+++ gradle-build-sample/src/test/java/example/ExampleTest.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.junit.Test;
+
+
+public class ExampleTest {
+
+    @Test
+    public void testJoin() {
+    		System.out.println("Yo what's up human! Oh you think you're fancy");
+    	}    
+
+}
